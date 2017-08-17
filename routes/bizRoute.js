c8e18454a240263f05a9519bcc875f2d8fbd0ccd
Damilare Joseph <dre4success@live.com>
2017-08-17 15:57:10 +0100

    routes and controller for business model

--- routes/bizRoute.js
+++ routes/bizRoute.js
@@ -8,5 +8,28 @@ module.exports = app => {
 		res.status(200).json(business);
 	});
 
-	app.get()
+	app.put("/api/business/:id", async (req, res) => {
+		const business = await Business.findOneAndUpdate(
+			{ _id: req.params.id },
+			req.body
+		).exec();
+
+		res.status(200).json(business);
+	});
+
+	app.delete("/api/business/:id", async (req, res) => {
+		const business = await Business.deleteOne({ _id: req.params.id });
+
+		res.status(200).send("Successfully deleted");
+	});
+
+	app.get("/api/business", async (req, res) => {
+		const business = await Business.find();
+		res.status(200).json(business);
+	});
+
+	app.get("/api/business/:id", async (req, res) => {
+		const business = await Business.findOne({ _id: req.params.id });
+		res.status(200).json(business);
+	});
 };
