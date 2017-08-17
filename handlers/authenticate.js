cf31903dc9dda9ee7c753533af9eb61a0e8396eb
Damilare Joseph <dre4success@live.com>
2017-08-17 23:54:45 +0100

    middleware to authenticate client

--- handlers/authenticate.js
+++ handlers/authenticate.js
@@ -1,2 +1,20 @@
-const mongoose = require('mongoose');
-const Client = mongoose.model('Client');
+const mongoose = require("mongoose");
+const Client = mongoose.model("Client");
+
+exports.authenticate = (req, res, next) => {
+	let token = req.header("x-auth");
+
+	Client.findByToken(token)
+		.then(client => {
+			if (!client) {
+				return Promise.reject();
+			}
+
+			req.client = client;
+			req.token = token;
+			next();
+		})
+		.catch(e => {
+			res.status(401).send();
+		});
+};
