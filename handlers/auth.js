0b203592e0fddbe2bbc9bc172ed71fb2ee7e15b6
Damilare Joseph <dre4success@live.com>
2017-08-18 09:31:05 +0100

    decoding token

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,8 +1,16 @@
 const jwt = require("jsonwebtoken");
 const keys = require("../config/dev");
+const expressJwt = require('express-jwt');
+checkToken = expressJwt({secret: keys.JWT_SECRET})
 
 exports.signToken = id => {
 	return jwt.sign({ _id: id }, keys.JWT_SECRET);
 };
 
-
+exports.decodeToken = (req, res, next) => {
+	checkToken, (req, res) => {
+		if (!req.user) return res.sendStatus(400);
+		res.sendStatus(200);
+	}
+	next();
+}
