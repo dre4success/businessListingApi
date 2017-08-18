1401bc540df3e3e9c41fd2b0f9ee90a48eb7b8c5
Damilare Joseph <dre4success@live.com>
2017-08-18 09:36:12 +0100

    adding delete ability for client

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Client = mongoose.model("Client");
-const _ = require("lodash");
 const auth = require("../handlers/auth");
 
 module.exports = app => {
@@ -11,8 +10,18 @@ module.exports = app => {
 		res.header("x-auth", req.body.token).send(client);
 	});
 
-	app.post('/api/client/login', async (req, res) => {
+	/*app.post('/api/client/login', async (req, res) => {
 		const client = await Client.findByCred(req.body.email, req.body.password);
 		res.header('x-auth', client.token).send(client);
+	})*/
+
+	app.delete('/api/client/:token', async (req, res) => {
+		const client = await Client.deleteOne({token: req.params.token})
+		res.status(200).json('successfully deleted');
+	})
+
+	app.get('/api/client', async (req, res) => {
+		const client = await Client.find()
+		res.status(200).json(client)
 	})
 };
