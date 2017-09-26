861d744a6f763486d875446fbe978c06bef935e4
Damilare <dre4success@live.com>
2017-09-26 20:21:11 +0100

    rewriting bcrypt encryption to use async/await

--- models/Client.js
+++ models/Client.js
@@ -1,69 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { Schema } = mongoose;
-const keys = require("../config/dev");
+const keys = require('../config/dev');
 
 const clientSchema = new Schema({
-	email: {
-		type: String,
-		required: "Please Enter Your Email Address",
-		trim: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 7
-	},
-	token: String
+  email: {
+    type: String,
+    required: 'Please Enter Your Email Address',
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7
+  }
 });
 
-/*clientSchema.statics.findByToken = function(token) {
-	var decoded
-	try {
-		decoded = jwt.verify(token, keys.JWT_SECRET);
-	} catch (e) {
-		return Promise.reject();
-	}
+clientSchema.pre('save', async function(next) {
+  const user = this;
 
-	return this.findOne({
-		_id: decoded._id,
-		token
-	});
-};*/
-
-clientSchema.statics.findByCred = function(email, password) {
-	return this.findOne({ email }).then(client => {
-		if (!client) {
-			return Promise.reject();
-		}
-
-		return new Promise((resolve, reject) => {
-			bcrypt.compare(password, client.password, (err, res) => {
-				if (res) {
-					resolve(client);
-				} else {
-					reject();
-				}
-			});
-		});
-	});
-};
-
-clientSchema.pre("save", function(next) {
-	if (this.isModified("password")) {
-		bcrypt.genSalt(10, (err, salt) => {
-			bcrypt.hash(this.password, salt, (err, hash) => {
-				this.password = hash;
-				next();
-			});
-		});
-	} else {
-		next();
-	}
+  // only hash password that has been modified
+  if (!user.isModified('password')) {
+    return next();
+  }
+  try {
+    // generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // hash the password with the new salt
+    const hash = await bcrypt.hash(user.password, salt);
+		//overide the cleartext password with the hashed one
+    user.password = hash;
+    next();
+  } catch (e) {
+    next(e);
+  }
 });
 
-module.exports = mongoose.model("Client", clientSchema);
+module.exports = mongoose.model('Client', clientSchema);
