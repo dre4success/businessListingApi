bc96ade33169f6de6cc635247930ad1d7a58cef9
Damilare <dre4success@live.com>
2017-09-27 08:00:15 +0100

    create signup and signin to send back token to the client

--- handlers/auth.js
+++ handlers/auth.js
@@ -8,13 +8,35 @@ const signToken = user => {
   return jwt.sign({ uid: user._id, iat: timestamp }, keys.JWT_SECRET);
 };
 
-exports.signUp = async (req, res, next) => {
-	
-	const email = req.body.email
-	const password = req.body.password
+exports.signUp = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-	if(!email || !password) {
-		return res.status(422).send({ error: 'You must provide email and password'});
-	}
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide email and password' });
+  }
 
-}
+  try {
+    // see if a user with given email exists
+    const existingClient = await Client.findOne({ email }).exec();
+
+    // if a client with email does exist, return an error
+    if (existingClient) {
+      res.status(422).send({ error: 'Email already exist' });
+    }
+
+    // if a client with the email does not exist, create new record
+    const client = await new Client({ email, password }).save();
+
+    res.json({ token: signToken(client) });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+exports.signIn = (req, res) => {
+  // when user signs in, we send them back the token
+  res.send({ token: signToken(req.client) });
+};
